8700ff62fc6bbde1eee5a4f62bd49f2036c20a9d
Matt Spurrier <matthew@senseict.com.au>
2020-08-22 01:43:41 +0800

    update

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ from distutils.core import setup
 
 setup(
     name='python-yay',
-    version='0.4.2',
+    version='0.0.1',
     description='Simple Python interface to Arch Linux package manager (yay)',
     author='Matt Spurrier',
     author_email='matthew@spurrier.com.au',
     url='https://github.com/digitalsparky/python-yay',
     py_modules=['yay'],
     keywords = ['yay', 'arch linux'],
-    download_url = 'https://github.com/digitalsparky/python-yay/archive/0.4.2.tar.gz',
+    download_url = 'https://github.com/digitalsparky/python-yay/archive/0.0.1.tar.gz',
     license = 'GPLv3',
     classifiers = [
     	"Development Status :: 3 - Alpha",
